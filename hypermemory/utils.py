53944633d539bb129f6707e90958a4aca62264e3
SimonBlanke <simonblanke@hotmail.de>
2020-04-02 12:28:50 +0200

    add utils + init

---
+++ hypermemory/utils.py
@@ -0,0 +1,28 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+import inspect
+import hashlib
+
+
+def get_func_str(function):
+    return inspect.getsource(function)
+
+
+def get_hash(object):
+    return hashlib.sha1(object).hexdigest()
+
+
+def get_model_id(model):
+    return str(get_hash(get_func_str(model).encode("utf-8")))
+
+
+def is_sha1(maybe_sha):
+    if len(maybe_sha) != 40:
+        return False
+    try:
+        sha_int = int(maybe_sha, 16)
+    except ValueError:
+        return False
+    return True
