390e8ea2903a19518200b4acb74642e053a4feb0
SimonBlanke <simonblanke@hotmail.de>
2020-05-26 13:08:41 +0200

    complete rewrite of MemoryDump class

--- hypermemory/memory_dump.py
+++ hypermemory/memory_dump.py
@@ -6,10 +6,12 @@ import os
 import json
 import dill
 import inspect
+import random
 
 import numpy as np
 import pandas as pd
 
+from operator import itemgetter
 from .memory_io import MemoryIO
 from .dataset_features import get_dataset_features
 from .utils import object_hash
@@ -28,6 +30,8 @@ class MemoryDump(MemoryIO):
             json.dump(_dict, json_file, indent=4)
 
     def dump_dataframe(self, _dataframe, path):
+        path = self._get_file_path(self.model)
+
         if os.path.exists(path):
             _dataframe_old = pd.read_csv(path)
 
@@ -46,6 +50,80 @@ class MemoryDump(MemoryIO):
             _dataframe_final = _dataframe
 
         _dataframe_final.to_csv(path, index=False)
+
+    def dump_short_term_memory(self, memory_dict, path):
+        self._search_space_types()
+        self._create_hash_list()
+
+        tuple_list = list(memory_dict.keys())
+        result_list = list(memory_dict.values())
+
+        results_df = pd.DataFrame(result_list)
+        np_pos = np.array(tuple_list)
+
+        para_dict = {}
+        for i, key in zip(range(np_pos.shape[1]), self.search_space):
+            np_pos_ = list(np_pos[:, i])
+            search_space_list = list(self.search_space[key])
+
+            if self.search_space_types[key] == "object":
+                search_space_list = self.object_hash_dict[key]
+
+            para_list = list(itemgetter(*np_pos_)(search_space_list))
+            para_dict[key] = para_list
+
+        para_df = pd.DataFrame(para_dict)
+        meta_data_df = pd.concat([para_df, results_df], axis=1)
+
+        self.dump_dataframe(meta_data_df, path)
+
+        print("\n self.object_hash_dict \n", self.object_hash_dict)
+
+    def _get_file_path(self, model_func):
+        if not os.path.exists(self.date_path):
+            os.makedirs(self.date_path)
+
+        return self.model_path + self.meta_data_name
+
+    def _search_space_types(self):
+        self.search_space_types = {}
+        for key in self.search_space.keys():
+            search_space_list = list(self.search_space[key])
+
+            # sampled_list = random.sample(aList, 3)
+
+            value = search_space_list[0]
+
+            if isinstance(value, int):
+                self.search_space_types[key] = "int"
+            elif isinstance(value, float):
+                self.search_space_types[key] = "float"
+            elif isinstance(value, str):
+                self.search_space_types[key] = "str"
+            else:
+                self.search_space_types[key] = "object"
+
+    def _create_hash_list(self):
+        self.object_hash_dict = {}
+
+        for key in self.search_space.keys():
+            if self.search_space_types[key] == "object":
+                search_space_list = list(self.search_space[key])
+
+                object_hash_list = []
+
+                for value in search_space_list:
+                    para_dill = dill.dumps(value)
+                    para_hash = object_hash(para_dill)
+
+                    with open(
+                        self.model_path + str(para_hash) + ".pkl", "wb"
+                    ) as pickle_file:
+                        dill.dump(para_dill, pickle_file)
+
+                    object_hash_list.append(para_hash)
+
+                self.object_hash_dict[key] = object_hash_list
 
 
 class MemoryDump1(MemoryIO):
