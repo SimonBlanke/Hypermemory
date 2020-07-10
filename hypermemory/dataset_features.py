0a4e52ead47391fcc6c365fc5b9ba631cf41fb8a
SimonBlanke <simonblanke@hotmail.de>
2020-07-10 08:30:53 +0200

    function now takes one array

--- hypermemory/dataset_features.py
+++ hypermemory/dataset_features.py
@@ -3,14 +3,10 @@
 # License: MIT License
 
 
-def get_dataset_features(X, y):
-    data = {
-        "X_size": X.size,
-        "X_byte_size": X.itemsize,
-        "X_ndim": X.ndim,
-        "y_size": y.size,
-        "y_byte_size": y.itemsize,
-        "y_ndim": y.ndim,
+def get_dataset_features(array):
+    return {
+        "array_size": array.size,
+        "array_byte_size": array.itemsize,
+        "array_ndim": array.ndim,
     }
 
-    return data
